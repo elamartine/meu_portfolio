ad84782898ba41a17b2a102063f80a2773484ad1
elamartine <estevam.nascimento@escolar.ifrn.edu.br>
2022-08-18 14:24:08 -0300

    ajustando o scroll

---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+// const menuItens = document.querySelectorAll(".menu a");
+
+// menuItens.forEach((item) => {
+//   item.addEventListener("click", scroll);
+// });
+
+// function scroll(event) {
+//   event.preventDefault();
+//   const element = event.target;
+//   const id = element.getAttribute('href');
+//   const to = document.querySelector(id).offsetTop;
+  
+//   window.scroll({
+//     top: to - 80,
+//     behavior: "smooth",
+//   });
+// }
+
